b4f3f29e5e1a7905bdcfac422a73fc4ce190fa1c
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-08-24 17:05:03 +0300

    structure the routes for the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 
 import './App.scss';
+import Header from './components/Header/Header'
+import Sidebar from './components/Sidebar/Sidebar'
+import {Home,Error,MealDetails,Category} from './pages/index'
+import {Route,Routes}from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Sidebar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/meal/:id' element={<MealDetails/>}/>
+        <Route path='/meal/category/:name' element={<Category/>}/>
+        <Route path='*' element={<Error/>}/>
+      </Routes>
     
     </div>
   );
